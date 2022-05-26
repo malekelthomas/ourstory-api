3d1d24a6e5022a289d58db2c6cf69657d737684e
Maleke Thomas <malekelthomas97@gmail.com>
2022-05-26 04:23:30 -0400

    added some models

---
+++ location/location.go
@@ -0,0 +1,8 @@
+package location
+
+//Geolocation information
+// @Description Latitude, Longitude Geolocation information
+type Location struct {
+	Latitude  float32 `json:"latitude"`
+	Longitude float32 `json:"longitude"`
+}
